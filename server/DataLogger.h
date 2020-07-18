1668104dc68701b3a3e9528f90c9a73f083e652c
MrPekar98 <martin.pekar@outlook.dk>
2020-07-18 23:03:56 +0200

    Interface for data logging.

---
+++ server/DataLogger.h
@@ -0,0 +1,14 @@
+#ifndef DATA_LOGGER_H
+#define DATA_LOGGER_H
+
+namespace ruslan
+{
+    // Interface for data logging.
+    class data_logger
+    {
+    public:
+        virtual ~data_logger();
+    };
+}
+
+#endif
