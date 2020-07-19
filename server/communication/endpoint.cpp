69a9164024ab238d53a18f46d7d9cc2720ff6d1c
MrPekar98 <martin.pekar@outlook.dk>
2020-07-19 14:50:28 +0200

    Added reading and writing functionality and operators.

--- server/communication/endpoint.cpp
+++ server/communication/endpoint.cpp
@@ -1,6 +1,46 @@
 #include "endpoint.h"
+#include <unistd.h>
+#include <string.h>
 
 namespace ruslan
 {
+    //endpoint::endpoint(int client_fd) : fd(client_fd) {}
 
+    // Sends message to endpoint.
+    int endpoint::writeto(void* buffer, unsigned len)
+    {
+        return write(this->fd, buffer, len);
+    }
+
+    // Reads message from endpoint into buffer.
+    int endpoint::readfrom(void* buffer, unsigned len)
+    {
+        return read(this->fd, buffer, len);
+    }
+
+    // Simple writing operator.
+    const endpoint& endpoint::operator<<(const std::string& str) const
+    {
+        write(this->fd, str.c_str(), str.length());
+        return *this;
+    }
+
+    // Simple reading operator.
+    template<typename T>
+    int endpoint::operator>>(T& buffer) const
+    {
+        T* buffer_read = new T[sizeof(T)];
+        int ret = read(this->fd, buffer_read, sizeof(buffer));
+
+        if (ret < 0)
+        {
+            delete[] buffer_read;
+            return ret;
+        }
+
+        buffer = *buffer_read;
+        delete[] buffer_read;
+
+        return ret;
+    }
 }
--- server/communication/endpoint.h
+++ server/communication/endpoint.h
@@ -1,11 +1,21 @@
 #include "communicatable.h"
+#include <string>
 
 namespace ruslan
 {
     class endpoint : public communicatable
     {
     public:
-        int writeto(const communicatable& dest, void* buffer, unsigned len) override;
-        int readfrom(const communicatable& src, void* buffer, unsigned len) override;
+        endpoint(int client_fd): fd(client_fd) {}
+        ~endpoint() {}
+        int writeto(void* buffer, unsigned len) override;
+        int readfrom(void* buffer, unsigned len) override;
+        const endpoint& operator<<(const std::string& str) const;
+
+        template<typename T>
+        int operator>>(T& buffer) const;
+
+    private:
+        int fd;
     };
 }
