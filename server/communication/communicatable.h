0580bdaebb3793b57e7cea7f9a03f76b5f89c84f
MrPekar98 <martin.pekar@outlook.dk>
2020-07-19 14:50:51 +0200

    Changed function signatures.

--- server/communication/communicatable.h
+++ server/communication/communicatable.h
@@ -8,8 +8,8 @@ namespace ruslan
     {
     public:
         virtual ~communicatable() {}
-        virtual int writeto(const communicatable& dest, void* buffer, unsigned len) = 0;
-        virtual int readfrom(const communicatable& src, void* buffer, unsigned len) = 0;
+        virtual int writeto(void* buffer, unsigned len) = 0;
+        virtual int readfrom(void* buffer, unsigned len) = 0;
     };
 }
 
