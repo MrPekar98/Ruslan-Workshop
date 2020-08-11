0413d83da38be56aef2f2232360127412fff6b84
MrPekar98 <martin.pekar@outlook.dk>
2020-08-11 15:55:56 +0200

    Implemented setters.

--- transmission/http.c
+++ transmission/http.c
@@ -1,4 +1,6 @@
 #include "http.h"
+#include <stdlib.h>
+#include <string.h>
 
 // Setter to port number.
 void http_set_port(struct http *RESTRICT request, unsigned port)
@@ -6,7 +8,7 @@ void http_set_port(struct http *RESTRICT request, unsigned port)
     request->port = port;
 }
 
-// Converts http struct into string.
+// Converts http struct into string. This is also used for GET requests.
 const char *http_str(struct http request)
 {
 
@@ -15,7 +17,8 @@ const char *http_str(struct http request)
 // Setter to URL.
 void http_set_url(struct http *RESTRICT request, const char *url)
 {
-
+    request->url = malloc(strlen(url) + 1);
+    strcpy(request->url, url);
 }
 
 // Adds header property to http request.
