7289fc7093dbf2909e4a99b99cec3b8510777820
MrPekar98 <martin.pekar@outlook.dk>
2020-08-11 15:49:59 +0200

    Added function declarations.

--- transmission/http.c
+++ transmission/http.c
@@ -1,1 +1,31 @@
 #include "http.h"
+
+// Setter to port number.
+void http_set_port(struct http *RESTRICT request, unsigned port)
+{
+    request->port = port;
+}
+
+// Converts http struct into string.
+const char *http_str(struct http request)
+{
+
+}
+
+// Setter to URL.
+void http_set_url(struct http *RESTRICT request, const char *url)
+{
+
+}
+
+// Adds header property to http request.
+void http_add_header_property(struct http *RESTRUCT request, const char *name, const char *value)
+{
+
+}
+
+// Creates a post request string from http struct and parameters.
+const char *http_post(struct http request, const char *body)
+{
+
+}
