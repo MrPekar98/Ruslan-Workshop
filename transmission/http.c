e5a63bbdeafd6d6e1255e628138716d704df8923
MrPekar98 <martin.pekar@outlook.dk>
2020-08-12 11:06:44 +0200

    Implemented setter to property header.

--- transmission/http.c
+++ transmission/http.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 // Setter to port number.
-void http_set_port(struct http *RESTRICT request, unsigned port)
+struct http http_init(unsigned port)
 {
-    request->port = port;
+    return (struct http) {.port = port, .property_count = 0};
 }
 
 // Converts http struct into string. This is also used for GET requests.
@@ -17,7 +17,16 @@ const char *http_str(struct http request)
 // Adds header property to http request.
 void http_add_header_property(struct http *RESTRICT request, const char *name, const char *value)
 {
+    if (request->property_count == 0)
+        request->header_properties = malloc(sizeof(struct property));
 
+    else
+        request->header_properties = realloc(request->header_properties, (request->property_count + 1) * sizeof(struct property));
+
+    struct property prop = {.name = malloc(strlen(name) + 1), .value = malloc(strlen(value) + 1)};
+    strcpy(prop.name, name);
+    strcpy(prop.value, value);
+    request->header_properties[request->property_count++] = prop;
 }
 
 // Creates a post request string from http struct and parameters.
