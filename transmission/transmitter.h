7eeadc3b5415fc7a77a373fc9b244b438df19d67
MrPekar98 <martin.pekar@outlook.dk>
2020-08-18 18:41:03 +0200

    Added final function args.

--- transmission/transmitter.h
+++ transmission/transmitter.h
@@ -1,15 +1,16 @@
 #ifndef TRANSMITTER_H
 #define TRANSMITTER_H
 
+#include <time.h>
+
 // Defines record of groups.
 typedef struct
 {
     char *name;
     unsigned balance, stock_count, stock_value, total_value;
 } group;
-
-// TODO: Abstract API call functions, such as buy and sell.
-int *get_market_data();     // TODO: Make struct for time that is easy to use.
+.
+int *get_market_data(struct tm from, struct tm to);
 group get_info();
 void buy();
 void sell();
